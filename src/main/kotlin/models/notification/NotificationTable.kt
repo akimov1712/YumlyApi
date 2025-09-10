2352ac36aace6cad40178c4ac23f2e32f63f1e1c
akimov1712 <akimov1712@mail.ru>
2025-09-10 20:08:09 +0300

    feat: impl crud operations for notificationTable

--- src/main/kotlin/models/notification/NotificationTable.kt
+++ src/main/kotlin/models/notification/NotificationTable.kt
@@ -1,8 +1,12 @@
 package ru.topbun.models.notification
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.kotlin.datetime.CurrentDateTime
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import ru.topbun.models.user.UserTable
 
 object NotificationTable: IntIdTable("notifications") {
@@ -12,5 +16,26 @@ object NotificationTable: IntIdTable("notifications") {
     val initiatorId = reference("user_id", UserTable)
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 
+    fun addNotification(userId: Int, type: NotificationType, initiatorId: Int) = transaction {
+        insert {
+            it[NotificationTable.userId] = userId
+            it[NotificationTable.type] = type.toString()
+            it[NotificationTable.initiatorId] = initiatorId
+        }
+    }
+
+    fun getNotifications(userId: Int, limit: Int, offset: Int) = transaction {
+        selectAll().where { NotificationTable.userId eq userId }
+            .limit(limit).offset(offset.toLong())
+            .map { it.toDTO() }
+    }
+
+
+    private fun ResultRow.toDTO() = NotificationDTO(
+        id = this[id].value,
+        type = NotificationType.valueOf(this[type]),
+        initiator = UserTable.getUser(this[initiatorId].value),
+        createdAt = this[createdAt]
+    )
 
 }
