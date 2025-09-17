0a14633b1f9918fcf640cf99eb66c457e6a0d044
topbun <akimov1712@mail.ru>
2025-09-17 18:11:48 +0300

    release: 1.2

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -9,7 +9,7 @@ import ru.topbun.plugins.configureSecurity
 import ru.topbun.plugins.configureSerialization
 
 fun main() {
-    embeddedServer(Netty, port = 80, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 3000, host = "127.0.0.1", module = Application::module)
         .start(wait = true)
 }
 
