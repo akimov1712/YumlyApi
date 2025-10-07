140685adf46ebe3e610226fc0a0274fcc4310645
topbun <akimov1712@mail.ru>
2025-10-07 14:35:48 +0300

    release: 1.3

--- src/main/kotlin/features/follow/entity/FollowersResponse.kt
+++ src/main/kotlin/features/follow/entity/FollowersResponse.kt
@@ -2,9 +2,10 @@ package ru.topbun.features.follow.entity
 
 import kotlinx.serialization.Serializable
 import models.user.UserDTO
+import ru.topbun.models.user.ProfileDTO
 
 @Serializable
 data class FollowersResponse(
     val count: Int,
-    val follows: List<UserDTO>
+    val follows: List<ProfileDTO>
 )
