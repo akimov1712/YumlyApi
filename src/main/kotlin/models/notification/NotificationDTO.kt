c046b2e16f6bf3ddff99081872a85f3bf1a2b471
topbun <akimov1712@mail.ru>
2025-10-16 13:08:34 +0300

    Release 1.4.3

--- src/main/kotlin/models/notification/NotificationDTO.kt
+++ src/main/kotlin/models/notification/NotificationDTO.kt
@@ -2,14 +2,14 @@ package ru.topbun.models.notification
 
 import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.Serializable
-import models.user.UserDTO
 import ru.topbun.models.recipe.RecipeDTO
+import ru.topbun.models.user.ProfileDTO
 
 @Serializable
 data class NotificationDTO(
     val id: Int,
     val type: NotificationType,
-    val initiator: UserDTO,
+    val initiator: ProfileDTO,
     val recipe: RecipeDTO?,
     val createdAt: LocalDateTime
 )
